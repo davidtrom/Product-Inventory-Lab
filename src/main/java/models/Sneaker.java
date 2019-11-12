41ced0a99cd501e16c3010e71a278fda28fb8869
David Trombello <david@MacBook-Pro-8.localdomain>
2019-11-12 13:52:55 -0500

    sneaker fields updated

---
+++ src/main/java/models/Sneaker.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Sneaker {
+    private Integer id;
+    private String name;
+    private String sport;
+    private Float size;
+    private Integer qty;
+    private Double price;
+
+
+}
